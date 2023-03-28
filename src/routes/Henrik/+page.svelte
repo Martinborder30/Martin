e7df89301e6c94c8818a5c8cde8c5cd2e2f2635b
henrikborder7 <henrik.nese@gmail.com>
2023-03-28 08:21:27 +0200

    Update +page.svelte

--- src/routes/Henrik/+page.svelte
+++ src/routes/Henrik/+page.svelte
@@ -1,60 +1,60 @@
 <div id="app">
-    <h1>Treningsprogram</h1>
-    <div class="exercise-list">
-      <h2>Øvelser</h2>
-      <ul>
-        <li v-for="exercise in exercises" :key="exercise.id">
-          {{ exercise.name }} ({{ exercise.duration }} sekunder)
-        </li>
-      </ul>
-    </div>
-    <div class="controls">
-      <button @click="startWorkout" :disabled="isWorkoutRunning">Start treningsøkt</button>
-      <button @click="stopWorkout" :disabled="!isWorkoutRunning">Stopp treningsøkt</button>
-    </div>
+  <h1>Treningsprogram</h1>
+  <div class="exercise-list">
+    <h2>Øvelser</h2>
+    <ul>
+      <li each={exercise in exercises} key={exercise.id}>
+        {exercise.name} ({exercise.duration} sekunder)
+      </li>
+    </ul>
   </div>
+  <div class="controls">
+    <button on:click={startWorkout} disabled={isWorkoutRunning}>Start treningsøkt</button>
+    <button on:click={stopWorkout} disabled={!isWorkoutRunning}>Stopp treningsøkt</button>
+  </div>
+</div>
 
 <script>
-    const exercises = [
-  { id: 1, name: "Knebøy", duration: 30 },
-  { id: 2, name: "Push-ups", duration: 30 },
-  { id: 3, name: "Sit-ups", duration: 30 }
+  const exercises = [
+{ id: 1, name: "Knebøy", duration: 30 },
+{ id: 2, name: "Push-ups", duration: 30 },
+{ id: 3, name: "Sit-ups", duration: 30 }
 ];
 
 const app = new App({
-  target: document.getElementById("app"),
-  data: {
-    exercises,
-    isWorkoutRunning: false,
-    intervalId: null
+target: document.getElementById("app"),
+data: {
+  exercises,
+  isWorkoutRunning: false,
+  intervalId: null
+},
+methods: {
+  startWorkout() {
+    this.set({ isWorkoutRunning: true });
+    const startTime = new Date().getTime();
+    let totalTime = 0;
+    let currentExerciseIndex = 0;
+    const tick = () => {
+      const currentTime = new Date().getTime();
+      const elapsedTime = currentTime - startTime;
+      totalTime += elapsedTime;
+      if (totalTime >= exercises[currentExerciseIndex].duration * 1000) {
+        currentExerciseIndex++;
+        totalTime = 0;
+      }
+      if (currentExerciseIndex >= exercises.length) {
+        clearInterval(this.get("intervalId"));
+        this.set({ isWorkoutRunning: false });
+      }
+    };
+    const intervalId = setInterval(tick, 100);
+    this.set({ intervalId });
   },
-  methods: {
-    startWorkout() {
-      this.set({ isWorkoutRunning: true });
-      const startTime = new Date().getTime();
-      let totalTime = 0;
-      let currentExerciseIndex = 0;
-      const tick = () => {
-        const currentTime = new Date().getTime();
-        const elapsedTime = currentTime - startTime;
-        totalTime += elapsedTime;
-        if (totalTime >= exercises[currentExerciseIndex].duration * 1000) {
-          currentExerciseIndex++;
-          totalTime = 0;
-        }
-        if (currentExerciseIndex >= exercises.length) {
-          clearInterval(this.get("intervalId"));
-          this.set({ isWorkoutRunning: false });
-        }
-      };
-      const intervalId = setInterval(tick, 100);
-      this.set({ intervalId });
-    },
-    stopWorkout() {
-      clearInterval(this.get("intervalId"));
-      this.set({ isWorkoutRunning: false });
-    }
+  stopWorkout() {
+    clearInterval(this.get("intervalId"));
+    this.set({ isWorkoutRunning: false });
   }
+}
 });
 
 </script>
@@ -62,29 +62,29 @@ const app = new App({
 <style>
 
 #app {
-  max-width: 600px;
-  margin: 0 auto;
-  text-align: center;
+max-width: 600px;
+margin: 0 auto;
+text-align: center;
 }
 
 .exercise-list {
-  margin-bottom: 20px;
+margin-bottom: 20px;
 }
 
 .controls button {
-  margin: 0 10px;
-  padding: 10px;
-  font-size: 16px;
-  cursor: pointer;
-  border: none;
-  background-color: #4CAF50;
-  color: white;
-  border-radius: 4px;
+margin: 0 10px;
+padding: 10px;
+font-size: 16px;
+cursor: pointer;
+border: none;
+background-color: #4CAF50;
+color: white;
+border-radius: 4px;
 }
 
 .controls button:disabled {
-  background-color: #ccc;
-  cursor: not-allowed;
+background-color: #ccc;
+cursor: not-allowed;
 }
 
 </style>
