f0026a3019b3b1cf5af31c2c1a2a69f76ff525a9
Martinborder30 <martinravndal2005@gmail.com>
2023-03-31 13:39:13 +0200

    Update +page.svelte

--- src/routes/Kristian/listeMedØkter/+page.svelte
+++ src/routes/Kristian/listeMedØkter/+page.svelte
@@ -1,3 +1,6 @@
+<body>
+<div id="hvaSkalTrene"><h1>Velg hva du vil trene</h1></div>
+
 <ul>
     <li>Full body</li>
         <ul>
@@ -15,3 +18,28 @@
 </ul>
 
 <div class="tilbake"><a href="/">Tilbake</a></div>
+</body>
+<style>
+html {
+    height: 100%;
+    margin: 0;
+}
+
+body{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    background:url("https://d12hfwo7xdmxn8.cloudfront.net/salespages/survey/tinypngopt-img/hero3.jpg") center;
+    background-size: cover;
+    background-attachment: fixed;
+    margin: 0;
+    height: 100vh;
+}
+#hvaSkalTrene{
+    width: 800px;
+    background-color: whitesmoke;
+    border-radius: 20px;
+    margin: ;
+}
+</style>
