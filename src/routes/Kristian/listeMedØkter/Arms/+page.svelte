0ef6494b6c6e474c7802188d80b01433ee4e1e1f
Crizzyborder30 <126252757+Crizzyborder30@users.noreply.github.com>
2023-04-14 14:19:56 +0200

    Update +page.svelte

---
+++ src/routes/Kristian/listeMedØkter/Arms/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+    import Tilbake from "../tilbake/tilbake.svelte"
+
+
+</script>
+
+<body>
+
+<h1>Push Day</h1>
+	<table>
+			<tr>
+				<th>Treningsøvelse</th>
+				<th>Antall sett</th>
+				<th>Antall repetisjoner</th>
+			</tr>
+			<tr>
+				<td>Triceps push-down</td>
+				<td>4</td>
+				<td>10-15</td>
+			</tr>
+			<tr>
+				<td>Bicep curls</td>
+				<td>4</td>
+				<td>10-15</td>
+			</tr>
+			<tr>
+				<td>Shoulder press</td>
+				<td>3</td>
+				<td>8-10</td>
+			</tr>
+			<tr>
+				<td>Hammer curls</td>
+				<td>3</td>
+				<td>8-12</td>
+			</tr>
+			<tr>
+				<td>Skull crushers</td>
+				<td>3</td>
+				<td>6-8</td>
+			</tr>
+            <tr>
+                <td>Lateral rasises</td>
+                <td>3</td>
+                <td></td>
+            </tr>
+	</table>
+</body>
+
+<Tilbake/>
+
+<style>
+    table{
+        border-collapse: collapse;
+    }
+    td, th{
+       border: 2px solid black; 
+    }
+    
+</style>
