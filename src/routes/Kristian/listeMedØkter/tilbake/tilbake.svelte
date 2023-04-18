2d54796558d09b93e5fb4710e8be96c668dcd734
Crizzyborder30 <126252757+Crizzyborder30@users.noreply.github.com>
2023-04-18 08:57:06 +0200

    Update tilbake.svelte

--- src/routes/Kristian/listeMedØkter/tilbake/tilbake.svelte
+++ src/routes/Kristian/listeMedØkter/tilbake/tilbake.svelte
@@ -1,1 +1,20 @@
-<div> <a href="/Kristian/listeMedØkter">Tilbake</a></div>
+<a href="/Kristian/listeMedØkter"><div class="tilbake"><i class="fa fa-backward" /></div></a>
+
+<style>
+      .tilbake {
+    position: absolute;
+    left: 20px;
+    top: 20px;
+    padding: 10px;
+    border-radius: 10px;
+    text-decoration: none;
+    color: white;
+  }
+  .tilbake i {
+    font-size: 5vh;
+    transition: 0.1s;
+  }
+  .tilbake i:hover {
+    font-size: 6vh;
+  }
+</style>
