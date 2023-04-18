65b90032468c5a85f22e80884ed01e40b6b754ae
henrikborder7 <henrik.nese@gmail.com>
2023-04-18 09:48:06 +0200

    lolers

--- src/routes/Henrik/+page.svelte
+++ src/routes/Henrik/+page.svelte
@@ -1,79 +1,141 @@
 <script>
-  let ikkeInnlogget = true;
-  let innlogget = false;
-  let innlogget2 = false;
-  let ferdig = false;
-  let godtaFullført = false;
-  let visKlokke = false
-  const side0 = () => {
-    ikkeInnlogget = true;
-    innlogget = false;
-    visKlokke = false;
-  };
+let ikkeInnlogget = true
+       let innlogget = false
+       let innlogget2 = false
+       let ferdig = false
+       let godtaFullført = false
+       let innlogget3 = false
+       let innlogget4 = false
+       let innlogget5 = false
+      const side0 =() => {
+         ikkeInnlogget =true
+         innlogget = false
+      }   
+   
+       const side1 = () => {
+          innlogget = true
+          innlogget2 = false
+          ikkeInnlogget =false
+         innlogget3= false
+          innlogget4 = false
+          innlogget5 = false
+       }
+       const side2 = () => {
+          innlogget2 = true
+          innlogget = false
+          ikkeInnlogget = false
+          godtaFullført= false
+          innlogget3= false
+          innlogget4 = false
+          innlogget5 = false
+          
+       }
+       const side3 = () => {
+          innlogget2 = false
+          innlogget = false
+          ikkeInnlogget = false
+          ferdig = false
+          godtaFullført = false
+          innlogget3 = true
+          innlogget4 = false
+          innlogget5 = false
+       }
+       const side4 = () => {
+          innlogget2 = false
+          innlogget = false
+          ikkeInnlogget = false
+          ferdig = false
+          godtaFullført = false
+          innlogget3 = false
+          innlogget4 = true
+          innlogget5 = false
+       }
+       const side5 = () => {
+          innlogget2 = false
+          innlogget = false
+          ikkeInnlogget = false
+          ferdig = false
+          godtaFullført = false
+          innlogget3 = false
+          innlogget4=false
+          innlogget5 = true
+       }
 
-  const side1 = () => {
-    innlogget = true;
-    innlogget2 = false;
-    ikkeInnlogget = false;
-    visKlokke = true;
-  };
-  const side2 = () => {
-    innlogget2 = true;
-    innlogget = false;
-    ikkeInnlogget = false;
-    godtaFullført = false;
-  };
-  const fullførtSjekk = () => {
-    innlogget2 = false;
-    innlogget = false;
-    ikkeInnlogget = false;
-    ferdig = false;
-    godtaFullført = true;
-  };
-  const ferdigside = () => {
-    innlogget2 = false;
-    innlogget = false;
-    ikkeInnlogget = false;
-    ferdig = true;
-    godtaFullført = false;
-  };
+       const fullførtSjekk = () => {
+          innlogget2 = false
+          innlogget = false
+          ikkeInnlogget = false
+          ferdig = false
+          godtaFullført =true
+          innlogget5 = false          
+       }
+       const ferdigside = () => {
+          innlogget2 = false
+          innlogget = false
+          ikkeInnlogget = false
+          ferdig = true
+          godtaFullført = false
+          
+       }
   import Stopwatch2 from "./Stopwatch/stopwatch2.svelte";
 </script>
 
 <div class="container">
     <div class="venstre">
           <div class="litenVenstre">
-      {#if ikkeInnlogget}
-        <button on:click={side1} id="startKnapp"> Start økt</button>
-      {/if}
-      {#if innlogget}
-        <h1>Øvelse 1</h1>
-        <p>Pushups</p>
-        <p>Antall repetisjoner: 20</p>
-        <div class="knappContainer">
-        <button on:click={side0}>Tilbake </button>
-        <button on:click={side2}>Neste øvelse </button>
-      </div>
-      {/if}
-      {#if innlogget2}
-        <h1>Øvelse 2</h1>
-        <p>Situps</p>
-        <p>Antall repetisjoner: 15</p>
-        <div class="knappContainer">
-        <button on:click={side1}>Tilbake </button>
-        <button on:click={fullførtSjekk}>Fullfør økt </button>
-      </div>
-      {/if}
-      {#if godtaFullført}
-        <h1>Er du helt sikker?</h1>
-        <button on:click={side2}>Avbryt </button>
-        <div class="knappContainer">
-        <button on:click={ferdigside}>Godta </button>
-      </div>
-      {/if}
-      {#if ferdig}
-        <h1>Bra jobba! Du er nå ferdig med økten.</h1>
-      {/if}
+			{#if ikkeInnlogget}
+   
+			<button on:click={side1}> Start økt</button>
+		   
+		{/if}
+		 {#if innlogget}
+			<h1>Øvelse 1</h1>
+			<p>Push-ups</p>
+			<p>Antall repetisjoner: 15-20</p>
+			<p>Antall set: 3</p>
+			<button on:click={side0}>Tilbake </button>
+			<button on:click={side2}>Neste øvelse </button>
+		 {/if}
+		  {#if innlogget2}
+			<h1>Øvelse 2</h1>
+			<p>Squats</p>
+			<p>Antall repetisjoner: 20</p>
+			<p>Antall set: 3</p>
+			<button on:click={side1}>Tilbake </button>
+		   <button on:click={side3}>Neste øvelse </button>
+		 {/if}
+		 {#if innlogget3}
+			<h1>Øvelse 3</h1>
+			<p>Utfall</p>
+			<p>Antall repetisjoner: 15(på hver side)</p>
+			<p>Antall set: 3</p>
+			<button on:click={side2}>Tilbake </button>
+		   <button on:click={side4}>Neste øvelse </button>
+		 {/if}
+		 {#if innlogget4}
+			<h1>Øvelse 4</h1>
+			<p>Sit-ups</p>
+			<p>Antall repetisjoner: 20</p>
+			<p>Antall set: 3</p>
+			<button on:click={side3}>Tilbake </button>
+		   <button on:click={side5}>Neste øvelse </button>
+		 {/if}
+		 {#if innlogget5}
+			<h1>Øvelse 5</h1>
+			<p>Planke</p>
+			<p>Antall repetisjoner: 60 sek</p>
+			<p>Antall set: 3</p>
+			<button on:click={side4}>Tilbake </button>
+		   <button on:click={fullførtSjekk}>Fullfør økt </button>
+		 {/if}
+		 {#if godtaFullført}
+			<h1>Er du helt sikker?</h1>
+			<button on:click={side5}>Avbryt </button>
+		   <button on:click={ferdigside}>Godta </button>
+		 {/if}
+		 {#if ferdig}
+		 <h1>Bra jobba! Du er nå ferdig med økten.</h1>
+	  {/if}
     </div>
 </div>
   
--- src/routes/Kristian/listeMedØkter/Fullbody1/+page.svelte
+++ src/routes/Kristian/listeMedØkter/Fullbody1/+page.svelte
@@ -52,3 +52,162 @@
     }
     
 </style>
+<script>
+       
+	let ikkeInnlogget = true
+	let innlogget = false
+	let innlogget2 = false
+	let ferdig = false
+	let godtaFullført = false
+	let innlogget3 = false
+	let innlogget4 = false
+	let innlogget5 = false
+   const side0 =() => {
+	  ikkeInnlogget =true
+	  innlogget = false
+   }   
+
+	const side1 = () => {
+	   innlogget = true
+	   innlogget2 = false
+	   ikkeInnlogget =false
+	  innlogget3= false
+	   innlogget4 = false
+	   innlogget5 = false
+	}
+	const side2 = () => {
+	   innlogget2 = true
+	   innlogget = false
+	   ikkeInnlogget = false
+	   godtaFullført= false
+	   innlogget3= false
+	   innlogget4 = false
+	   innlogget5 = false
+	   
+	}
+	const side3 = () => {
+	   innlogget2 = false
+	   innlogget = false
+	   ikkeInnlogget = false
+	   ferdig = false
+	   godtaFullført = false
+	   innlogget3 = true
+	   innlogget4 = false
+	   innlogget5 = false
+	}
+	const side4 = () => {
+	   innlogget2 = false
+	   innlogget = false
+	   ikkeInnlogget = false
+	   ferdig = false
+	   godtaFullført = false
+	   innlogget3 = false
+	   innlogget4 = true
+	   innlogget5 = false
+	}
+	const side5 = () => {
+	   innlogget2 = false
+	   innlogget = false
+	   ikkeInnlogget = false
+	   ferdig = false
+	   godtaFullført = false
+	   innlogget3 = false
+	   innlogget4=false
+	   innlogget5 = true
+	}
+
+	const fullførtSjekk = () => {
+	   innlogget2 = false
+	   innlogget = false
+	   ikkeInnlogget = false
+	   ferdig = false
+	   godtaFullført =true
+	   innlogget5 = false          
+	}
+	const ferdigside = () => {
+	   innlogget2 = false
+	   innlogget = false
+	   ikkeInnlogget = false
+	   ferdig = true
+	   godtaFullført = false
+	   
+	}
+  
+ </script>
+ <body>
+{#if ikkeInnlogget}
+
+	<button on:click={side1}> Start økt</button>
+   
+{/if}
+ {#if innlogget}
+	<h1>Øvelse 1</h1>
+	<p>Push-ups</p>
+	<p>Antall repetisjoner: 15-20</p>
+	<p>Antall set: 3</p>
+	<button on:click={side0}>Tilbake </button>
+	<button on:click={side2}>Neste øvelse </button>
+ {/if}
+  {#if innlogget2}
+	<h1>Øvelse 2</h1>
+	<p>Squats</p>
+	<p>Antall repetisjoner: 20</p>
+	<p>Antall set: 3</p>
+	<button on:click={side1}>Tilbake </button>
+   <button on:click={side3}>Neste øvelse </button>
+ {/if}
+ {#if innlogget3}
+	<h1>Øvelse 3</h1>
+	<p>Utfall</p>
+	<p>Antall repetisjoner: 15(på hver side)</p>
+	<p>Antall set: 3</p>
+	<button on:click={side2}>Tilbake </button>
+   <button on:click={side4}>Neste øvelse </button>
+ {/if}
+ {#if innlogget4}
+	<h1>Øvelse 4</h1>
+	<p>Sit-ups</p>
+	<p>Antall repetisjoner: 20</p>
+	<p>Antall set: 3</p>
+	<button on:click={side3}>Tilbake </button>
+   <button on:click={side5}>Neste øvelse </button>
+ {/if}
+ {#if innlogget5}
+	<h1>Øvelse 5</h1>
+	<p>Planke</p>
+	<p>Antall repetisjoner: 60 sek</p>
+	<p>Antall set: 3</p>
+	<button on:click={side4}>Tilbake </button>
+   <button on:click={fullførtSjekk}>Fullfør økt </button>
+ {/if}
+ {#if godtaFullført}
+	<h1>Er du helt sikker?</h1>
+	<button on:click={side5}>Avbryt </button>
+   <button on:click={ferdigside}>Godta </button>
+ {/if}
+ {#if ferdig}
+ <h1>Bra jobba! Du er nå ferdig med økten.</h1>
+{/if}
+</body>
+<style>
+button {
+background-color: #007bff;
+color: white;
+padding: 10px;
+border-radius: 5px;
+font-size: 16px;
+margin: 10px;
+border: none;
+cursor: pointer;
+}
+
+h1 {
+font-size: 24px;
+margin-bottom: 10px;
+}
+
+p {
+font-size: 18px;
+margin-bottom: 5px;
+}
+</style>
